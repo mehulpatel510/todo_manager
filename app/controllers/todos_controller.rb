07cfa45c717b86c57b7d4cabebfc18983cdda273
mehulpatel510 <mspatel.it@spcevng.ac.in>
2021-09-12 21:23:25 +0530

    Implemented update method with PATCH http request by Mehul Patel

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -22,5 +22,12 @@ class TodosController < ApplicationController
       response_text = "New todo created with id #{new_todo.id}"
       render plain: response_text
     end
-
+    def update
+      id = params[:id]
+      completed = params[:completed]
+      todo = Todo.find(id)
+      todo.completed = completed
+      todo.save!
+      render plain: "Completed #{id} todo to #{completed}."
+    end
   end
