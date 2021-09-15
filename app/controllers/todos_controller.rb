e76011d1551e1ce32eb18215f10face1c4d1b2c2
mehulpatel510 <mspatel.it@spcevng.ac.in>
2021-09-15 23:59:28 +0530

    Deploy todo manam=gement app on heroku

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,37 +1,35 @@
 class TodosController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-    def index
-      render "index"
-      # render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
-    end
-
-    def show
-      id = params[:id]
-      todo = Todo.find(id)
-      render plain: todo.to_pleasant_string
-    end
-    
-    def create
-      todo_text = params[:todo_text]
-      due_date = DateTime.parse(params[:due_date])
-      completed = false
-      new_todo = Todo.create!(
-        todo_text: todo_text,
-        due_date: due_date,
-        completed: completed
-      )
-      response_text = "New todo created with id #{new_todo.id}"
-      render plain: response_text
-    end
-    
-    def update
-      id = params[:id]
-      completed = params[:completed]
-      todo = Todo.find(id)
-      todo.completed = completed
-      todo.save!
-      render plain: "Completed #{id} todo to #{completed}."
-    end
-    
+  def index
+    render "index"
   end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    completed = false
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: completed,
+    )
+    response_text = "New todo created with id #{new_todo.id}"
+    render plain: response_text
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Completed #{id} todo to #{completed}."
+  end
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,28 @@
 class Todo < ActiveRecord::Base
-    def to_pleasant_string
-        is_completed = completed ? "[X]" : "[ ]"
-        "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+    def due_today?
+        due_date == Date.today
+    end
+    
+    def self.overdue
+        where("due_date < ?", Date.today)
+    end
+
+    def self.due_today
+        where("due_date = ?", Date.today)
+    end
+
+    def self.due_later
+        where("due_date > ?", Date.today)
+    end
+
+    def self.add_task(todo)
+        Todo.create!(todo_text:todo[:todo_text],due_date: (Date.today + todo[:due_in_days]), completed:false)
+    end
+  
+    def self.mark_as_complete(todo_id)
+        todo = Todo.find(todo_id)
+        todo.completed = true
+        todo.save
+        todo
     end
 end
