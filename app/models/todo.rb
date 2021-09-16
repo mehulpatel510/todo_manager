2d1619d18bd4939dfba06fe18a3c02797f08030e
mehulpatel510 <mspatel.it@spcevng.ac.in>
2021-09-16 13:56:31 +0530

    Use ERB to render todo page and deploy to Heroku

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,30 +1,31 @@
 class Todo < ActiveRecord::Base
-    def due_today?
-        due_date == Date.today
-    end
-    def self.completed
-        where(completed:true)
-    end
-    def self.overdue
-        where("due_date < ? and completed = ?" , Date.today, false)
-    end
+  def due_today?
+    due_date == Date.today
+  end
 
-    def self.due_today
-        where("due_date = ?", Date.today)
-    end
+  def self.completed
+    where(completed: true)
+  end
+  def self.overdue
+    where("due_date < ? and completed = ?", Date.today, false)
+  end
 
-    def self.due_later
-        where("due_date > ?", Date.today)
-    end
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
 
-    def self.add_task(todo)
-        Todo.create!(todo_text:todo[:todo_text],due_date: (Date.today + todo[:due_in_days]), completed:false)
-    end
-  
-    def self.mark_as_complete(todo_id)
-        todo = Todo.find(todo_id)
-        todo.completed = true
-        todo.save
-        todo
-    end
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.add_task(todo)
+    Todo.create!(todo_text: todo[:todo_text], due_date: (Date.today + todo[:due_in_days]), completed: false)
+  end
+
+  def self.mark_as_complete(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
+  end
 end
