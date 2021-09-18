9b49888e659447d9c1b596095f70fa1ad68d575d
mehulpatel510 <mspatel.it@spcevng.ac.in>
2021-09-18 23:40:00 +0530

    Build and deploy the final Todo Manager application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_165623) do
+ActiveRecord::Schema.define(version: 2021_09_18_180852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 2021_09_18_165623) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
+    t.text "first_name"
+    t.text "last_name"
+    t.text "email"
+    t.text "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
